1a4f118e43502dcffff517405a7a1600ddb8a685
Salvatore Cirone <s.cirone@reply.it>
2023-03-09 18:18:34 +0100

    initiating random

--- src/strategies/random_sg.py
+++ src/strategies/random_sg.py
@@ -1,4 +1,26 @@
+from game.snake import Snake
+import random
+
+
 def play_random(snakes, matrix):
 
-    for s in snakes:
-        pass
+    for snake in snakes:
+        play_snake(snake)
+
+
+def play_snake(snake: Snake, matrix):
+
+    is_wh = True
+
+    init_row = random.randint(0, len(matrix))
+    init_col = random.randint(0, len(matrix[0]))
+
+    while is_wh:
+        cell = matrix[init_row][init_col]
+
+        is_wh = cell.wh
+
+    snake.starting_cell(init_row, init_col)
+
+    while snake.snake_length > 0:
+        direction = random.randint(0, 3)
