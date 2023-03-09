5df627467c5c33aac5886cb379f0c5c67d4715ed
Damiano Fisicaro <da.fisicaro@reply.it>
2023-03-09 18:18:19 +0100

    add compute snake score

--- src/game/snake.py
+++ src/game/snake.py
@@ -1,18 +1,26 @@
+from cell import Cell, Wormhole
 class Snake:
     starting_cell = list()
     moves = list()
+    cells = list()
 
     def __init__(self, snake_length):
         self.snake_length = snake_length
 
     def set_starting_cell(self, col, row):
-        self.starting_cell = (row, col)
+        self.starting_cell = (col, row)
+        self.cells.append(self.starting_cell)
 
-    def move_snake(self, direction, wormhole_coords=None):
-        if wormhole_coords:
+    def move_snake(self, direction, cell: Cell | Wormhole):
+        if cell.wh:
             self.moves.append(direction)
-            self.moves.append(wormhole_coords)
+            self.moves.append((cell.col, cell.row))
         else:
             self.moves.append(direction)
+        self.cells.append(cell)
+    
+    def compute_score(self):
+        return sum([c.value for c in self.cells])
+
     def __repr__(self):
         return str(self.snake_length)
