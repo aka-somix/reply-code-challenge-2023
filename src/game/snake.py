ed39dda1cd21988f0d896aa695acadc32cc65d23
Salvatore Cirone <s.cirone@reply.it>
2023-03-09 17:46:33 +0100

    snake update

--- src/game/snake.py
+++ src/game/snake.py
@@ -6,10 +6,11 @@ class Snake:
         self.snake_length = snake_length
 
     def set_starting_cell(self, col, row):
-        self.starting_cell = [col, row]
+        self.starting_cell = (row, col)
 
     def move_snake(self, direction, wormhole_coords=None):
-        if wormhole_coords is not None:
-            self.moves.apppend([direction, wormhole_coords])
+        if wormhole_coords:
+            self.moves.append(direction)
+            self.moves.append(wormhole_coords)
         else:
             self.moves.append(direction)
