03f7c7af12ed91ee30c2a1dadc8fc182b5d23463
Salvatore Cirone <s.cirone@reply.it>
2023-03-09 17:04:08 +0100

    cell updated

--- src/game/cell.py
+++ src/game/cell.py
@@ -1,5 +1,28 @@
 class Cell:
-    def __init__(self, row, col, value):
+
+    def __init__(self, row, col, value, nrows, ncols):
         self.row = row
         self.col = col
+        self.nrows = nrows
+        self.ncols = ncols
         self.value = value
+
+    def up(self):
+        r = (self.row + 1) % self.nrows
+        c = self.col
+        return (r, c)
+
+    def down(self):
+        r = (self.row + self.nrows - 1) % self.nrows
+        c = self.col
+        return (r, c)
+
+    def right(self):
+        r = self.row
+        c = (self.col + 1) % self.ncols
+        return (r, c)
+
+    def left(self):
+        r = self.row
+        c = (self.col - 1 + self.ncols) % self.ncols
+        return (r, c)
